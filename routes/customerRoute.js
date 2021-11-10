2563c4fffc3d1d4d1faba075267395c6b3b79d81
TabishAK <takchamp6@gmail.com>
2021-11-10 11:58:59 +0500

    gmail issue

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -53,11 +53,15 @@ app.post("/signup", async (req, res) => {
       style="cursor: pointer; max-width: 35%; height: auto; position: relative !important; left: -55px; margin-bottom: -28px !important;"
       />
       <div style="border-bottom:dotted 1px #444"><br></div></td></tr><tr><td><p style="font-family:arial; font-size:10pt; color:#828282">
-    <span style="font-size:16px; font-weight:bold">Hello ${newCustomer.first_name}!</span><br></p>
+    <span style="font-size:16px; font-weight:bold">Hello ${
+      newCustomer.first_name
+    }${" "}${newCustomer.last_name}!</span><br></p>
     <span style="font-size:16px; font-weight:bold">EFF Website registration</span><br></p>
     <div class="x_translatedBlock">Thankyou for registering with the Exclusive Fabrics Website.</div><p></p></td></tr><tr><td>
     <p>Please click the link below to verify your account<p>
-    <a href="http://54.183.217.110/customerAuth/verify-email?token=${newCustomer.emailToken}&slugForBroucher=${req.body.slugForBroucher}"> Verify your account </a>
+    <a clicktracking="off" href="http://54.183.217.110/customerAuth/verify-email?token=${
+      newCustomer.emailToken
+    }&slugForBroucher=${req.body.slugForBroucher}"> Verify your account </a>
     <br/>
     <div style="border-top:dotted 1px #444"><br></div><p style="font-family:arial; font-size:7pt; color:#828282">Confidentiality Notice:<br><br><span class="x_translatedBlock">The information in this email is confidential and is intended solely for the addressee. If you are not the intended recipient, you must not read, use or disseminate the information contained. Any views expressed in this message are those of the individual sender, except where the sender specifically states them to be the views of Romo Ltd. Romo Ltd owns the intellectual property rights and copyright of all images and products relating to the Romo, Black Edition, Villa Nova, Mark Alexander, Zinc Textile and Kirkby Design brands of furnishing fabrics, wallcoverings and trimmings. Any unauthorised reproduction or copying of Romo Ltd products or images may result in legal action. Romo Ltd is a Limited Company registered in England and Wales. The registered office address is:  Exclusive Fabrics | 440 Boulder Court, Suite 100, Pleasanton, CA 94566</span>
     </p></td></tr>
