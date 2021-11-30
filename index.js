150fd788408a4382acb85224c4d522963315ef8e
TabishAK <takchamp6@gmail.com>
2021-11-30 14:45:10 +0500

    Wahi cors :(

--- index.js
+++ index.js
@@ -23,16 +23,12 @@ mongoose.connect(
   }
 );
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 50000,
-  })
-);
 
 app.use("/banner", banner);
 app.use("/products", products);
