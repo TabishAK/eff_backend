f9c5cb37e7477b7e011809e406bd78648c62e10a
TabishAK <takchamp6@gmail.com>
2021-11-30 13:14:26 +0500

    Big issue

--- index.js
+++ index.js
@@ -24,13 +24,8 @@ mongoose.connect(
   }
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 app.use(cors());
-
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
