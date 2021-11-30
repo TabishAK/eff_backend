08eafb6a969d61cbd11f64c9d2d7607e36d62928
TabishAK <takchamp6@gmail.com>
2021-11-30 12:38:46 +0500

    cors issues

--- index.js
+++ index.js
@@ -24,13 +24,16 @@ mongoose.connect(
   }
 );
 
-app.use(function (req, res, next) {
+var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
+};
+
+app.configure(function () {
+  app.use(allowCrossDomain);
+  //some other code
 });
 
 app.use(cors());
